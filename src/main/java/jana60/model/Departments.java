28885031ed6392d9b4c63dc4d7078fa411a43fc1
matteocovetti <covetti.matteo@gmail.com>
2022-07-14 16:26:04 +0200

    creato repository

--- src/main/java/jana60/model/Departments.java
+++ src/main/java/jana60/model/Departments.java
@@ -12,7 +12,7 @@ public class Departments {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 
 	private String name;
 
@@ -28,11 +28,11 @@ public class Departments {
 
 	// getter e setter
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
---
+++ src/main/java/jana60/repository/DepartmentsRepository.java
@@ -0,0 +1,11 @@
+package jana60.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jana60.model.Departments;
+
+@Repository
+public interface DepartmentsRepository extends CrudRepository<Departments, Integer> {
+
+}
