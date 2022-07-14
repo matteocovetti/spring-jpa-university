42acd00b638f6ee399daf215711ba067ea288833
matteocovetti <covetti.matteo@gmail.com>
2022-07-14 16:21:00 +0200

    creato model

--- src/main/java/jana60/model/Departments.java
+++ src/main/java/jana60/model/Departments.java
@@ -1,5 +1,87 @@
 package jana60.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
 public class Departments {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String address;
+
+	private String phone;
+
+	private String email;
+
+	private String website;
+
+	private String headOfDepartment;
+
+	// getter e setter
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getHeadOfDepartment() {
+		return headOfDepartment;
+	}
+
+	public void setHeadOfDepartment(String headOfDepartment) {
+		this.headOfDepartment = headOfDepartment;
+	}
 
 }
