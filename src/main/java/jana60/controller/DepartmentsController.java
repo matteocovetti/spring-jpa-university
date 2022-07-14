082b109fed6fa1f1896cb630f9a80ad676de3300
matteocovetti <covetti.matteo@gmail.com>
2022-07-14 16:35:55 +0200

    esercizio bonus finito

--- src/main/java/jana60/controller/DepartmentsController.java
+++ src/main/java/jana60/controller/DepartmentsController.java
@@ -1,10 +1,14 @@
 package jana60.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jana60.model.Departments;
 import jana60.repository.DepartmentsRepository;
 
 @Controller
@@ -15,7 +19,9 @@ public class DepartmentsController {
 	private DepartmentsRepository repo;
 
 	@GetMapping
-	public String departments() {
+	public String departments(Model model) {
+		List<Departments> departmentList = (List<Departments>) repo.findAll();
+		model.addAttribute("departmentList", departmentList);
 		return "departments";
 	}
 }
